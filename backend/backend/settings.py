fa2a46973c30573ace67e5f6b0c4d4d35345cc83
shivasai322 <dakurishivasai@gmailcom>
2026-02-05 20:57:24 +0530

    event

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-2n&=4@k8qwng#4%s-f%nj#_s36dm0fw3=%alqh8o(gco8%n!@8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
